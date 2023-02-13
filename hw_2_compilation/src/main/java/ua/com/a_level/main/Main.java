0c7057f2f53e2beef0147d0b5857c50549b4a5e6
Posokhov Nikita <itposokhovnikita@gmail.com>
2023-02-13 21:58:16 +0200

    hw_1

---
+++ hw_2_compilation/src/main/java/ua/com/a_level/main/Main.java
@@ -0,0 +1,12 @@
+package ua.com.a_level.main;
+
+import ua.com.a_level.menu.Menu;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Menu menu = new Menu();
+        menu.start();
+
+    }
+}
