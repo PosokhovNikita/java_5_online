770cc70739382d313dd485c0e161f5411de8b9a2
Posokhov Nikita <itposokhovnikita@gmail.com>
2023-06-13 13:36:19 +0300

    hw_8, 9

---
+++ hw_8_compilation/src/main/java/ua/com/a_level/Main.java
@@ -0,0 +1,17 @@
+package ua.com.a_level;
+
+import ua.com.a_level.controller.Controller;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Controller controller = new Controller();
+        try {
+            controller.start();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
